3703512e442c920fe3485919526e11b2b6e08db1
廖昱晴 <pulin.liao@ctbcsec.com>
2021-02-25 13:56:50 +0800

    - init alert

--- uiAlertController/ViewController.swift
+++ uiAlertController/ViewController.swift
@@ -14,6 +14,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showAlert(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Hello", message: "How are you?", preferredStyle: .actionSheet)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        let helloAction = UIAlertAction(title: "Say Hello", style: .destructive) { (action) in
+            print("Say Hello")
+            self.dismiss(animated: true, completion: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            print("Cancel")
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        alert.addAction(okAction)
+        alert.addAction(helloAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
